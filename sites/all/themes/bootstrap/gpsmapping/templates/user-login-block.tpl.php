e7d7070b2043174d48a203d269654e6fa9dfb0c4
ahmedzbeda <zbeda@Ahmeds-MacBook-Pro.local>
2014-10-25 13:36:34 +0200

    new updates

---
+++ sites/all/themes/bootstrap/gpsmapping/templates/user-login-block.tpl.php
@@ -0,0 +1,19 @@
+<div id="user-login-block-container">
+  <div id="user-login-block-form-fields">
+	  <div class="row">
+		  <div class="col-md-6">
+			    <?php print $name; ?>
+			    <?php print $pass; ?>
+			    <?php print $submit; ?>
+			    <?php print $rendered; ?>
+		  </div>
+		  <div class="col-md-6">
+			  <div class="links">
+			    <p><a href="/user/register">Create an Account</a></p>
+			    <p><a href="/user/password">Forgot Password</a></p>
+			  </div>			  
+		  </div>		  
+
+	  </div>
+  </div>
+</div>
