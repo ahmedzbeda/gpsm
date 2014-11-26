9c2782646d1cc4d4f089989d79a5aa4c6d303208
ahmedzbeda <0@0.ly>
2014-11-26 11:55:20 +0200

    devices

---
+++ sites/all/themes/bootstrap/gpsmapping/templates/user-login.tpl.php
@@ -0,0 +1,13 @@
+<div class="row">
+	<div class="col-sm-6" style="border-right: 1px solid #eee;">
+		<h2>Login</h2>		
+		<?php print drupal_render_children($form) ?>
+	</div>
+	<div class="col-sm-6">
+		<h2 style="margin-bottom: 40px;">Other Options ...</h2>
+			  <div class="links">
+			    <p style="margin-bottom: 30px;"><a style="margin-right: 10px; display: block; float: left;" class="btn-lg btn-success" href="/user/register">Create an Account</a></p>
+			    <p style="margin-bottom: 30px;"><a style="margin-right: 10px; display: block; float: left;" class="btn-lg btn-success" href="/user/password">Forgot Password</a></p>
+			  </div>			  				
+	</div>	
+</div>
